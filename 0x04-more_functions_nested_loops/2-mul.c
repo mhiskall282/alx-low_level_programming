6819e8702a2f63d71be1311527fbb2530bc009e9
John Okyere <johnokyere282@icloud.com>
2023-06-22 18:27:59 -0700

    w

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  * mul - multiple two integers
+ *   * @a: integer to perform the operation
+ *    * @b: int to perform the operation
+ *     * Return: the result of the operation
+ *      */
+
+int mul(int a, int b)
+{
+		return (a * b);
+}
