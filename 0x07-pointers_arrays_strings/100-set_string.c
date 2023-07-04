8983412b3761409f64f3825e1f1099094b30e222
John Okyere <johnokyere282@icloud.com>
2023-07-03 19:04:11 -0700

    work

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+include "main.h"
+/**
+ * set_string - set pointer to char
+ * @s: pointer to store
+ * @to: string to change
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
