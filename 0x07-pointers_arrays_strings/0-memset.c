5f93cd5a55c897a09d7c848a38318ed41e97c2c4
John Okyere <johnokyere282@icloud.com>
2023-07-04 07:37:15 -0700

    work

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
-* _memset - copy files by constant
-* @s: variable to store
-* @b: variable to be copyed
-* @n: constant
-*
-* Return: s
-*/
+ * _memset - copy files by constant
+ * @s: variable to store
+ * @b: variable to be copyed
+ * @n: constant
+ *
+ * Return: s
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-		unsigned int i;
+	unsigned int i;
 
-			for (i = 0; i < n; i++)
-					{
-								s[i] = b;
-									}
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-				return (s);
+	return (s);
 }
