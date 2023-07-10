b00abcdb63cd3885380fbf2b94bb9d979356acab
John Okyere <johnokyere282@icloud.com>
2023-07-10 10:23:32 -0700

    work

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *main - print number of argument passed
+ * @argc: int argument
+ * @argv; string pointer
+ *
+ * Return: 0;
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
