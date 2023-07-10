00ebd00d4dba011742af5fd2749ca96a3e1898cb
John Okyere <johnokyere282@icloud.com>
2023-07-10 10:12:49 -0700

    work

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the program name
+ * @argc: int argument
+ * @argv:string pointer
+ *
+ * Return:0
+ */
+
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
