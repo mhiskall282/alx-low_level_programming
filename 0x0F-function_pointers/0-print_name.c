fed0fddc2b37f01e0baf02f65d060f3c90508a45
John Okyere <johnokyere282@icloud.com>
2023-07-19 18:30:21 -0700

    work

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - print name
+ * @name: name argument
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
